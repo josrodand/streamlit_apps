bb3fbd6bb31e2e33cf6525266cd3d0260f56683f
Selu <josrodand@gmail.com>
2020-12-31 11:54:58 +0100

    Primera prueba de aplicacion: test_app.py, lectura de datos financieros

--- test_app.py
+++ test_app.py
@@ -1,1 +1,19 @@
-print("hola mundo")
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+
+st.write("""
+# Simple stock Price App
+
+Shown are the stock closing price and volume of Google!
+
+""")
+
+tickerSymbol = "GOOGL"
+# sacamos datos de yfinance
+tickerData = yf.Ticker(tickerSymbol)
+# generamos datos historicos
+tickerDF = tickerData.history(period = 'id', start = '2010-5-31', end = '2020-5-31')
+
+st.line_chart(tickerDF.Close)
+st.line_chart(tickerDF.Volume)
